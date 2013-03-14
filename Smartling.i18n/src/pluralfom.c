60a210a43a45aa1ed9daed7b9b634d8c376d8662
Pavel Ivashkov <pavels.code@gmail.com>
2013-03-14 02:49:18 +0200

    + plural rules updated to CLDR 22.1

--- Smartling.i18n/src/pluralfom.c
+++ Smartling.i18n/src/pluralfom.c
@@ -11,13 +11,41 @@ const char* pluralformf(const char* lang, float n);
 // Generated from CLDR Version 21
 // http://unicode.org/cldr/trac/browser/tags/release-21/common/supplemental/plurals.xml
 // http://unicode.org/repos/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html
-
 const char* pluralformf(const char* lang, float n)
 {
-	// TODO: handle fraction numbers
 	if (n == (int)n) {
 		return pluralform(lang, (int)n);
 	}
+
+	if (!lang)
+		return "other";
+
+	unsigned int lc = 0;
+	for (; *lang; lang++) { lc = (lc << 8) + *lang; }
+
+	switch (lc) {
+
+	// set6
+	case 0x736869: // shi
+		if ((n >= 0 && n <= 1)) // n within 0..1
+			return "one";
+		break;
+
+	// set11
+	case 0x6c6167: // lag
+		if (((n >= 0 && n <= 2)) && (n != 0) && (n != 2)) // n within 0..2 and n is not 0 and n is not 2
+			return "one";
+		break;
+
+	// set19
+	case 0x6666: // ff
+	case 0x6672: // fr
+	case 0x6b6162: // kab
+		if (((n >= 0 && n <= 2)) && (n != 2)) // n within 0..2 and n is not 2
+			return "one";
+		break;
+	}
+
 	return "other";
 }
 
@@ -25,75 +53,43 @@ const char* pluralform(const char* lang, int n)
 {
 	if (!lang)
 		return "other";
-	
+
 	unsigned int lc = 0;
 	for (; *lang; lang++) { lc = (lc << 8) + *lang; }
 
 	switch (lc) {
 
 	// set1
-	case 0x6b7368: // ksh
-		if (n == 1) // n is 1
+	case 0x6c74: // lt
+		if (((n % 10) == 1) && (((n % 100) < 11 || (n % 100) > 19))) // n mod 10 is 1 and n mod 100 not in 11..19
 			return "one";
-		if (n == 0) // n is 0
-			return "zero";
+		if ((((n % 10) >= 2 && (n % 10) <= 9)) && (((n % 100) < 11 || (n % 100) > 19))) // n mod 10 in 2..9 and n mod 100 not in 11..19
+			return "few";
 		break;
 
 	// set2
-	case 0x6379: // cy
-		if (n == 2) // n is 2
-			return "two";
-		if (n == 6) // n is 6
-			return "many";
-		if (n == 3) // n is 3
-			return "few";
-		if (n == 1) // n is 1
-			return "one";
+	case 0x6c76: // lv
 		if (n == 0) // n is 0
 			return "zero";
+		if (((n % 10) == 1) && ((n % 100) != 11)) // n mod 10 is 1 and n mod 100 is not 11
+			return "one";
 		break;
 
 	// set3
-	case 0x736c: // sl
-		if ((n % 100) == 2) // n mod 100 is 2
+	case 0x6379: // cy
+		if (n == 2) // n is 2
 			return "two";
-		if ((n % 100) == 1) // n mod 100 is 1
-			return "one";
-		if (((n % 100) >= 3 && (n % 100) <= 4)) // n mod 100 in 3..4
+		if (n == 3) // n is 3
 			return "few";
+		if (n == 0) // n is 0
+			return "zero";
+		if (n == 1) // n is 1
+			return "one";
+		if (n == 6) // n is 6
+			return "many";
 		break;
 
 	// set4
-	case 0x6776: // gv
-		if ((((n % 10) >= 1 && (n % 10) <= 2)) || ((n % 20) == 0)) // n mod 10 in 1..2 or n mod 20 is 0
-			return "one";
-		break;
-
-	// set5
-	case 0x6761: // ga
-		if (n == 2) // n is 2
-			return "two";
-		if (n == 1) // n is 1
-			return "one";
-		if ((n >= 3 && n <= 6)) // n in 3..6
-			return "few";
-		if ((n >= 7 && n <= 10)) // n in 7..10
-			return "many";
-		break;
-
-	// set6
-	case 0x6272: // br
-		if (((n % 1000000) == 0) && (n != 0)) // n mod 1000000 is 0 and n is not 0
-			return "many";
-		if (((n % 10) == 2) && (((n % 100) != 12) && ((n % 100) != 72) && ((n % 100) != 92))) // n mod 10 is 2 and n mod 100 not in 12,72,92
-			return "two";
-		if (((n % 10) == 1) && (((n % 100) != 11) && ((n % 100) != 71) && ((n % 100) != 91))) // n mod 10 is 1 and n mod 100 not in 11,71,91
-			return "one";
-		if ((((n % 10) >= 3 && (n % 10) <= 4) || ((n % 10) == 9)) && (((n % 100) < 10 || (n % 100) > 19) && ((n % 100) < 70 || (n % 100) > 79) && ((n % 100) < 90 || (n % 100) > 99))) // n mod 10 in 3..4,9 and n mod 100 not in 10..19,70..79,90..99
-			return "few";
-		break;
-
-	// set7
 	case 0x6265: // be
 	case 0x6273: // bs
 	case 0x6872: // hr
@@ -109,7 +105,72 @@ const char* pluralform(const char* lang, int n)
 			return "many";
 		break;
 
+	// set5
+	case 0x6b7368: // ksh
+		if (n == 0) // n is 0
+			return "zero";
+		if (n == 1) // n is 1
+			return "one";
+		break;
+
+	// set6
+	case 0x736869: // shi
+		if ((n >= 2 && n <= 10)) // n in 2..10
+			return "few";
+		if ((n >= 0 && n <= 1)) // n within 0..1
+			return "one";
+		break;
+
+	// set7
+	case 0x6865: // he
+		if (n == 2) // n is 2
+			return "two";
+		if (n == 1) // n is 1
+			return "one";
+		if ((n != 0) && ((n % 10) == 0)) // n is not 0 AND n mod 10 is 0
+			return "many";
+		break;
+
 	// set8
+	case 0x6373: // cs
+	case 0x736b: // sk
+		if (n == 1) // n is 1
+			return "one";
+		if ((n >= 2 && n <= 4)) // n in 2..4
+			return "few";
+		break;
+
+	// set9
+	case 0x6272: // br
+		if ((n != 0) && ((n % 1000000) == 0)) // n is not 0 and n mod 1000000 is 0
+			return "many";
+		if (((n % 10) == 1) && (((n % 100) != 11) && ((n % 100) != 71) && ((n % 100) != 91))) // n mod 10 is 1 and n mod 100 not in 11,71,91
+			return "one";
+		if (((n % 10) == 2) && (((n % 100) != 12) && ((n % 100) != 72) && ((n % 100) != 92))) // n mod 10 is 2 and n mod 100 not in 12,72,92
+			return "two";
+		if ((((n % 10) >= 3 && (n % 10) <= 4) || ((n % 10) == 9)) && (((n % 100) < 10 || (n % 100) > 19) && ((n % 100) < 70 || (n % 100) > 79) && ((n % 100) < 90 || (n % 100) > 99))) // n mod 10 in 3..4,9 and n mod 100 not in 10..19,70..79,90..99
+			return "few";
+		break;
+
+	// set10
+	case 0x736c: // sl
+		if ((n % 100) == 2) // n mod 100 is 2
+			return "two";
+		if ((n % 100) == 1) // n mod 100 is 1
+			return "one";
+		if (((n % 100) >= 3 && (n % 100) <= 4)) // n mod 100 in 3..4
+			return "few";
+		break;
+
+	// set11
+	case 0x6c6167: // lag
+		if (n == 0) // n is 0
+			return "zero";
+		if (((n >= 0 && n <= 2)) && (n != 0) && (n != 2)) // n within 0..2 and n is not 0 and n is not 2
+			return "one";
+		break;
+
+	// set12
 	case 0x706c: // pl
 		if (n == 1) // n is 1
 			return "one";
@@ -119,29 +180,7 @@ const char* pluralform(const char* lang, int n)
 			return "many";
 		break;
 
-	// set9
-	case 0x6666: // ff
-	case 0x6672: // fr
-	case 0x6b6162: // kab
-		if (((n >= 0 && n <= 2)) && (n != 2)) // n within 0..2 and n is not 2
-			return "one";
-		break;
-
-	// set10
-	case 0x6172: // ar
-		if (n == 2) // n is 2
-			return "two";
-		if (n == 1) // n is 1
-			return "one";
-		if (n == 0) // n is 0
-			return "zero";
-		if (((n % 100) >= 3 && (n % 100) <= 10)) // n mod 100 in 3..10
-			return "few";
-		if (((n % 100) >= 11 && (n % 100) <= 99)) // n mod 100 in 11..99
-			return "many";
-		break;
-
-	// set11
+	// set13
 	case 0x6764: // gd
 		if ((n == 2) || (n == 12)) // n in 2,12
 			return "two";
@@ -151,21 +190,19 @@ const char* pluralform(const char* lang, int n)
 			return "few";
 		break;
 
-	// set12
-	case 0x747a6d: // tzm
-		if (((n >= 0 && n <= 1)) || ((n >= 11 && n <= 99))) // n in 0..1 or n in 11..99
-			return "one";
-		break;
-
-	// set13
-	case 0x6c6167: // lag
-		if (n == 0) // n is 0
-			return "zero";
-		if (((n >= 0 && n <= 2)) && (n != 0) && (n != 2)) // n within 0..2 and n is not 0 and n is not 2
-			return "one";
-		break;
-
 	// set14
+	case 0x6776: // gv
+		if ((((n % 10) >= 1 && (n % 10) <= 2)) || ((n % 20) == 0)) // n mod 10 in 1..2 or n mod 20 is 0
+			return "one";
+		break;
+
+	// set15
+	case 0x6d6b: // mk
+		if (((n % 10) == 1) && (n != 11)) // n mod 10 is 1 and n is not 11
+			return "one";
+		break;
+
+	// set16
 	case 0x6d74: // mt
 		if (n == 1) // n is 1
 			return "one";
@@ -175,7 +212,7 @@ const char* pluralform(const char* lang, int n)
 			return "few";
 		break;
 
-	// set15
+	// set17
 	case 0x6d6f: // mo
 	case 0x726f: // ro
 		if (n == 1) // n is 1
@@ -184,7 +221,27 @@ const char* pluralform(const char* lang, int n)
 			return "few";
 		break;
 
-	// set16
+	// set18
+	case 0x6761: // ga
+		if (n == 2) // n is 2
+			return "two";
+		if (n == 1) // n is 1
+			return "one";
+		if ((n >= 3 && n <= 6)) // n in 3..6
+			return "few";
+		if ((n >= 7 && n <= 10)) // n in 7..10
+			return "many";
+		break;
+
+	// set19
+	case 0x6666: // ff
+	case 0x6672: // fr
+	case 0x6b6162: // kab
+		if (((n >= 0 && n <= 2)) && (n != 2)) // n within 0..2 and n is not 2
+			return "one";
+		break;
+
+	// set20
 	case 0x6975: // iu
 	case 0x6b77: // kw
 	case 0x7365: // se
@@ -200,30 +257,7 @@ const char* pluralform(const char* lang, int n)
 			return "one";
 		break;
 
-	// set17
-	case 0x6d6b: // mk
-		if (((n % 10) == 1) && (n != 11)) // n mod 10 is 1 and n is not 11
-			return "one";
-		break;
-
-	// set18
-	case 0x6373: // cs
-	case 0x736b: // sk
-		if (n == 1) // n is 1
-			return "one";
-		if ((n >= 2 && n <= 4)) // n in 2..4
-			return "few";
-		break;
-
-	// set19
-	case 0x6c74: // lt
-		if (((n % 10) == 1) && (((n % 100) < 11 || (n % 100) > 19))) // n mod 10 is 1 and n mod 100 not in 11..19
-			return "one";
-		if ((((n % 10) >= 2 && (n % 10) <= 9)) && (((n % 100) < 11 || (n % 100) > 19))) // n mod 10 in 2..9 and n mod 100 not in 11..19
-			return "few";
-		break;
-
-	// set20
+	// set21
 	case 0x616b: // ak
 	case 0x616d: // am
 	case 0x6268: // bh
@@ -240,7 +274,13 @@ const char* pluralform(const char* lang, int n)
 			return "one";
 		break;
 
-	// set21
+	// set22
+	case 0x747a6d: // tzm
+		if (((n >= 0 && n <= 1)) || ((n >= 11 && n <= 99))) // n in 0..1 or n in 11..99
+			return "one";
+		break;
+
+	// set23
 	case 0x6166: // af
 	case 0x6267: // bg
 	case 0x626e: // bn
@@ -261,12 +301,13 @@ const char* pluralform(const char* lang, int n)
 	case 0x676c: // gl
 	case 0x6775: // gu
 	case 0x6861: // ha
-	case 0x6865: // he
 	case 0x6973: // is
 	case 0x6974: // it
 	case 0x6b6b: // kk
 	case 0x6b6c: // kl
+	case 0x6b73: // ks
 	case 0x6b75: // ku
+	case 0x6b79: // ky
 	case 0x6c62: // lb
 	case 0x6c67: // lg
 	case 0x6d6c: // ml
@@ -282,6 +323,7 @@ const char* pluralform(const char* lang, int n)
 	case 0x6e79: // ny
 	case 0x6f6d: // om
 	case 0x6f72: // or
+	case 0x6f73: // os
 	case 0x7061: // pa
 	case 0x7073: // ps
 	case 0x7074: // pt
@@ -300,23 +342,30 @@ const char* pluralform(const char* lang, int n)
 	case 0x7473: // ts
 	case 0x7572: // ur
 	case 0x7665: // ve
+	case 0x766f: // vo
 	case 0x7868: // xh
 	case 0x7a75: // zu
 	case 0x617361: // asa
+	case 0x617374: // ast
 	case 0x62656d: // bem
 	case 0x62657a: // bez
 	case 0x627278: // brx
 	case 0x636767: // cgg
 	case 0x636872: // chr
+	case 0x636b62: // ckb
 	case 0x667572: // fur
 	case 0x677377: // gsw
 	case 0x686177: // haw
+	case 0x6a676f: // jgo
 	case 0x6a6d63: // jmc
 	case 0x6b616a: // kaj
 	case 0x6b6367: // kcg
+	case 0x6b6b6a: // kkj
 	case 0x6b7362: // ksb
 	case 0x6d6173: // mas
+	case 0x6d676f: // mgo
 	case 0x6e6168: // nah
+	case 0x6e6e68: // nnh
 	case 0x6e796e: // nyn
 	case 0x706170: // pap
 	case 0x726f66: // rof
@@ -334,20 +383,18 @@ const char* pluralform(const char* lang, int n)
 			return "one";
 		break;
 
-	// set22
-	case 0x736869: // shi
-		if ((n >= 2 && n <= 10)) // n in 2..10
-			return "few";
-		if ((n >= 0 && n <= 1)) // n within 0..1
+	// set24
+	case 0x6172: // ar
+		if (n == 2) // n is 2
+			return "two";
+		if (n == 1) // n is 1
 			return "one";
-		break;
-
-	// set23
-	case 0x6c76: // lv
 		if (n == 0) // n is 0
 			return "zero";
-		if (((n % 10) == 1) && ((n % 100) != 11)) // n mod 10 is 1 and n mod 100 is not 11
-			return "one";
+		if (((n % 100) >= 3 && (n % 100) <= 10)) // n mod 100 in 3..10
+			return "few";
+		if (((n % 100) >= 11 && (n % 100) <= 99)) // n mod 100 in 11..99
+			return "many";
 		break;
 	}
 
